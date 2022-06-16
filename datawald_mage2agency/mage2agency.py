930dc100b47f26a876530f2d63764833aaf6cd6c
DESKTOP-ENENB64\Bibo W\github <ideabosque@gmail.com>
2022-06-16 11:12:27 -0700

    Update insert_update_assets.

--- datawald_mage2agency/mage2agency.py
+++ datawald_mage2agency/mage2agency.py
@@ -26,46 +26,58 @@ class Mage2Agency(Agency):
     def insert_update_assets(self, assets):
         for asset in assets:
             tx_type = asset.get("tx_type_src_id").split("-")[0]
-            sku = asset.get("tx_type_src_id").replace(f"{tx_type}-", "")
             try:
                 if tx_type == "product":
-                    self.mage2Connector.insert_update_product(
-                        sku,
-                        asset["data"].get("attribute_set", "default"),
-                        asset["data"],
-                        asset["data"].get("type_id"),
-                        asset["data"].get("store_id", 0),
-                    )
-                    if len(asset["data"].get("stock_data", {})) > 0:
-                        self.mage2Connector.insert_update_cataloginventory_stock_item(
-                            sku,
-                            asset["data"].get("stock_data"),
-                            asset["data"].get("store_id", 0),
-                        )
-
-                    if len(asset["data"].get("category_data", [])) > 0:
-                        self.mage2Connector.insert_update_categories(
-                            sku, asset["data"].get("category_data")
-                        )
-
-                    if len(asset["data"].get("tier_price_data", [])) > 0:
-                        self.mage2Connector.insert_update_product_tier_price(
-                            sku,
-                            asset["data"].get("tier_price_data"),
-                            asset["data"].get("store_id", 0),
-                        )
-
-                    if asset["data"].get("variant_data"):
-                        self.mage2Connector.insert_update_variant(
-                            sku,
-                            asset["data"].get("variant_data"),
-                            asset["data"].get("store_id", 0),
-                        )
-
+                    tgt_id = self.insert_update_product(asset)
                 else:
                     raise Exception(f"TX Type ({tx_type}) is not supported!!!")
+
+                asset.update(
+                    {
+                        "tx_status": "S",
+                        "tx_note": f"datawald -> {asset['target']}",
+                        "tgt_id": tgt_id,
+                    }
+                )
             except Exception:
                 log = traceback.format_exc()
-                # asset.update({"tx_status": "F", "tx_note": log, "tgt_id": "####"})
+                asset.update({"tx_status": "F", "tx_note": log, "tgt_id": "####"})
                 self.logger.exception(log)
         return assets
+
+    def insert_update_product(self, asset):
+        sku = asset.get("tx_type_src_id").replace(f"product-", "")
+        product_id = self.mage2Connector.insert_update_product(
+            sku,
+            asset["data"].get("attribute_set", "default"),
+            asset["data"],
+            asset["data"].get("type_id"),
+            asset["data"].get("store_id", 0),
+        )
+        if len(asset["data"].get("stock_data", {})) > 0:
+            self.mage2Connector.insert_update_cataloginventory_stock_item(
+                sku,
+                asset["data"].get("stock_data"),
+                asset["data"].get("store_id", 0),
+            )
+
+        if len(asset["data"].get("category_data", [])) > 0:
+            self.mage2Connector.insert_update_categories(
+                sku, asset["data"].get("category_data")
+            )
+
+        if len(asset["data"].get("tier_price_data", [])) > 0:
+            self.mage2Connector.insert_update_product_tier_price(
+                sku,
+                asset["data"].get("tier_price_data"),
+                asset["data"].get("store_id", 0),
+            )
+
+        if asset["data"].get("variant_data"):
+            self.mage2Connector.insert_update_variant(
+                sku,
+                asset["data"].get("variant_data"),
+                asset["data"].get("store_id", 0),
+            )
+
+        return product_id
